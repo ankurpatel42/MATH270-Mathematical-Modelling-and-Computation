00ac3f5fccd43b8481a4b2ac08b88b376246aeb0
Ankur Patel <akp41@uclive.ac.nz>
2021-08-11 22:27:19 +1200

    implemented method of solving without using built in MATLAB functions, also changed using the built-in 'inv' function to using A\b as this is two to three times faster

--- Lab_4_Iterative_Methods_for_Linear_Systems/guassseidel.m
+++ Lab_4_Iterative_Methods_for_Linear_Systems/guassseidel.m
@@ -3,11 +3,16 @@ function [results, converge_flag] = guassseidel(A, b, x, niter, tol)
     D = diag(diag(A));
     L = tril(A, -1);
     U = triu(A, 1);
-    inverse_L_D = inv(L + D);
+    M = (L + D)\(-U); % this represents x^(k+1) = g(x^(k)) and the properties of the matrix M determine convergence
+    c = (L + D)\b;
     converge_flag = 0;
     for k = 1:niter
-        M = inverse_L_D * (-U); % this represents x^(k+1) = g(x^(k)) and the properties of the matrix M determine convergence
-        next_x = M*x + inverse_L_D*b;
+        % For a 3x3 matrix
+        % next_x = transpose(zeros(1, 3));
+        % next_x(1) = (1/D(1,1)) * (-(0)-(U(1,2)*x(2) + U(1,3)*x(3)) + b(1) );
+        % next_x(2) = (1/D(2,2)) * (-(L(2,1)*next_x(1) + U(2,3)*x(3)) + b(2) );
+        % next_x(3) = (1/D(3,3)) * (-(L(3,1)*next_x(1) + L(3,2)*next_x(2)) - (0) + b(3) );
+        next_x = M*x + c;
         if (norm(next_x - x, inf) / norm(next_x, inf)) < tol
             converge_flag = 1;
             results(k, :) = [k, x(1), x(2), x(3)];
