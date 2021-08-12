5ca6401773d3beca433e6c47fb469f88a59ae0d6
Ankur Patel <63583759+ankurpatel42@users.noreply.github.com>
2021-08-13 11:29:45 +1200

    use 'end' to find end of results

--- Lab_4_Iterative_Methods_for_Linear_Systems/gsconvergence.m
+++ Lab_4_Iterative_Methods_for_Linear_Systems/gsconvergence.m
@@ -1,6 +1,6 @@
 A = [4, 1, 1; 1, 2, 3; 2, -1, -3];
 % A = [4, -2, 1; 3, -6, 1; -4, 1, 6];
-% A = [1, -2, 1; 3, 1, 1; -4, 1, 1]; Did not converge, diagonal values in
+% A = [1, -2, 1; 3, 1, 1; -4, 1, 1]; %Did not converge, diagonal values in
 % column is NOT.... and the matrix norm || M || p is not < 1
 b = [5; 5; -3];
 x0 = [0; 0; 0];
@@ -15,17 +15,9 @@ niter = 100;
 [results, converge_flag] = guassseidel(A, b, x0, niter, tol);
 
 [m, n] = size(results);
-for i = 1:m
-    if results(i) == 0
-        iter = results(i-1);
-        break
-    else
-        iter = m;
-    end
-end
 
 if converge_flag == 1
-    fprintf('Converged to x = (%.4f, %.4f, %.4f) in %d iterations\n', results(iter,2), results(iter, 3), results(iter, 4), iter);
+    fprintf('Converged to x = (%.4f, %.4f, %.4f) in %d iterations\n', results(end, 2:4), m);
 else
     fprintf('Failed to converge. Number of iterations carried out = %d\n', iter);
 end
