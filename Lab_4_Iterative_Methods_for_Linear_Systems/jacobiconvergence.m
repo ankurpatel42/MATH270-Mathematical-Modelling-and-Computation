0fc4f81faa5870118424f8c44b475921f030f06a
Ankur Patel <63583759+ankurpatel42@users.noreply.github.com>
2021-08-13 11:27:43 +1200

    use 'end' to cutoff zeros in results

--- Lab_4_Iterative_Methods_for_Linear_Systems/jacobiconvergence.m
+++ Lab_4_Iterative_Methods_for_Linear_Systems/jacobiconvergence.m
@@ -3,22 +3,14 @@ b = [5; 5; -3];
 x0 = [0; 0; 0];
 tol = 1e-6; % Convergence
 niter = 100;
-%tol = 1e-9; No convergence
-%niter = 100;
+% tol = 1e-9; % No convergence
+% niter = 100;
 [results, converge_flag] = jacobi(A, b, x0, niter, tol);
-
 [m, n] = size(results);
-for i = 1:m
-    if results(i) == 0
-        iter = results(i-1);
-        break
-    else
-        iter = m;
-    end
-end
+
 
 if converge_flag == 1
-    fprintf('Converged to x = (%.4f, %.4f, %.4f) in %d iterations\n', results(iter,2), results(iter, 3), results(iter, 4), iter);
+    fprintf('Converged to x = (%.4f, %.4f, %.4f) in %d iterations\n', results(end, 2:4), m);
 else
-    fprintf('Failed to converge. Number of iterations carried out = %d\n', iter);
+    fprintf('Failed to converge. Number of iterations carried out = %d\n', m);
 end
