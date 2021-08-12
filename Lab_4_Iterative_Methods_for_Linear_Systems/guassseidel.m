d64d7e3972914ee1f45d1e16527779480d2607b9
Ankur Patel <63583759+ankurpatel42@users.noreply.github.com>
2021-08-13 11:26:45 +1200

    use explicit equation for 3x3 matrix

--- Lab_4_Iterative_Methods_for_Linear_Systems/guassseidel.m
+++ Lab_4_Iterative_Methods_for_Linear_Systems/guassseidel.m
@@ -1,18 +1,18 @@
 function [results, converge_flag] = guassseidel(A, b, x, niter, tol)
     results = zeros(niter, 4);
-    D = diag(diag(A));
-    L = tril(A, -1);
-    U = triu(A, 1);
-    M = (L + D)\(-U); % this represents x^(k+1) = g(x^(k)) and the properties of the matrix M determine convergence
-    c = (L + D)\b;
+%     D = diag(diag(A));
+%     L = tril(A, -1);
+%     U = triu(A, 1);
+%     M = (L + D)\(-U); % this represents x^(k+1) = g(x^(k)) and the properties of the matrix M determine convergence
+%     c = (L + D)\b;
     converge_flag = 0;
     for k = 1:niter
         % For a 3x3 matrix
-        % next_x = transpose(zeros(1, 3));
-        % next_x(1) = (1/D(1,1)) * (-(0)-(U(1,2)*x(2) + U(1,3)*x(3)) + b(1) );
-        % next_x(2) = (1/D(2,2)) * (-(L(2,1)*next_x(1) + U(2,3)*x(3)) + b(2) );
-        % next_x(3) = (1/D(3,3)) * (-(L(3,1)*next_x(1) + L(3,2)*next_x(2)) - (0) + b(3) );
-        next_x = M*x + c;
+        next_x = transpose(zeros(1, 3));
+        next_x(1) = (1/A(1,1)) * (-(0)-(A(1,2)*x(2) + A(1,3)*x(3)) + b(1) );
+        next_x(2) = (1/A(2,2)) * (-(A(2,1)*next_x(1) + A(2,3)*x(3)) + b(2) );
+        next_x(3) = (1/A(3,3)) * (-(A(3,1)*next_x(1) + A(3,2)*next_x(2)) - (0) + b(3) );
+%         next_x = M*x + c;
         if (norm(next_x - x, inf) / norm(next_x, inf)) < tol
             converge_flag = 1;
             results(k, :) = [k, x(1), x(2), x(3)];
